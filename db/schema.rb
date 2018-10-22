3a7e8437cad6d34a31f11effb31bbff783f78fcf
Lukas Kuhn <lukaskuhn.lku@gmail.com>
2018-10-22 17:44:33 +0200

    Created User Model and Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_180447) do
+ActiveRecord::Schema.define(version: 2018_10_22_154156) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2018_10_21_180447) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
