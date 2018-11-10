e43ab236cd6fae68fd2000f4dd0345b5d2326f56
Lukas Kuhn <lukaskuhn.lku@gmail.com>
2018-11-10 22:13:44 +0100

    Revert "Devise generate migration User"
    This reverts commit 525e674977dd9a61e692deaf8a63bdf0fb3cc40b.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
     before_save {self.email = email.downcase}
     
     validates :username, presence: true, uniqueness: {case_sensitive: false},
