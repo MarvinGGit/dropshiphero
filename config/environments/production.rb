10a284997c06a1182662b9d9f6e5c32fbb2b9710
Lukas Kuhn <lukaskuhn.lku@gmail.com>
2018-11-11 17:11:59 +0100

    changed email so it works in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,9 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   #Devise Config
-  config.action_mailer.default_url_options = { host: 'dropshiphero.herokuapp.com', port: 80 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'dropshiphero.herokuapp.com', :protocol => 'https' }
+
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
