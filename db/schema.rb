15f520c70de226bd65c09407818b87c26641af36
Lukas Kuhn <lukaskuhn.lku@gmail.com>
2018-11-11 20:27:22 +0100

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_11_192005) do
+ActiveRecord::Schema.define(version: 2018_11_11_192555) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2018_11_11_192005) do
     t.integer "shares"
     t.integer "comments"
     t.datetime "facebookad_posted_at"
+    t.string "facebookad_url"
+    t.string "review_csv_url"
   end
 
   create_table "users", force: :cascade do |t|
