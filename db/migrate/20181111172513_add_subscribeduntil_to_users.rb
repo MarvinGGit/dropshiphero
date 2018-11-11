27aa0bdb5060f05af03ab916751a390aac6b867a
Lukas Kuhn <lukaskuhn.lku@gmail.com>
2018-11-11 18:29:05 +0100

    db migrate

--- db/migrate/20181111172513_add_subscribeduntil_to_users.rb
+++ db/migrate/20181111172513_add_subscribeduntil_to_users.rb
@@ -1,5 +1,5 @@
 class AddSubscribeduntilToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_colum :users, :subscribed_until, :datetime
+    add_column :users, :subscribed_until, :datetime
   end
 end
