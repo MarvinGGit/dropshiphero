0f6205647346ead535749ffad0a6e3abf39aa867
Lukas Kuhn <lukaskuhn.lku@gmail.com>
2018-11-16 06:42:51 +0100

    changed model because of error

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,4 @@
 class Product < ApplicationRecord
     has_many :product_categories
     has_many :categories, through: :product_categories
-
-    validates :sellprice, presence: true
-    validates :buyprice, presence: true
-    validates :storeepacketprice, presence: true
 end
